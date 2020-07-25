199c2e181259da4cfa09a6f1deae47b62697bfbb
Mohamed Adel <mohamedadel2015ar@gmail.com>
2020-07-25 08:51:47 +0200

    reservation final step

--- app/Http/Controllers/reservecontroller.php
+++ app/Http/Controllers/reservecontroller.php
@@ -13,16 +13,16 @@ use DB;
 
 class reservecontroller extends Controller
 {
-    public function show( $id)
+    public function show($id)
     {
         //$reservation = reservations::All();
-/*             try {
-                $reserve = Trip::find($id);
-                return view('reservations')->with('reserve', $reserve);
-            } catch (ModelNotFoundException $exception) {
-                abort(404);
-            } */
-     // return view('reservations', compact('trip'));
+        /*             try {
+                        $reserve = Trip::find($id);
+                        return view('reservations')->with('reserve', $reserve);
+                    } catch (ModelNotFoundException $exception) {
+                        abort(404);
+                    } */
+        // return view('reservations', compact('trip'));
 
 
     }
@@ -33,97 +33,119 @@ class reservecontroller extends Controller
         $users = User::all();
         $trip = trip::find($id);
 
-/*         $reserve = DB::table('reservation')
-                    ->join('users','reservation.user_id','users.id')
-                    ->join('trip','reservation.trip_id','trip.id')
-                    ->get(); */
-        return view('reservations',compact('reserve','users','trip'));
-
+        /*         $reserve = DB::table('reservation')
+                            ->join('users','reservation.user_id','users.id')
+                            ->join('trip','reservation.trip_id','trip.id')
+                            ->get(); */
+        return view('reservations', compact('reserve', 'users', 'trip'));
 
 
     }
-    public function confirm(Request $request,$id)
+
+    public function confirm(Request $request, $id)
     {
 
         $trip = trip::find($id);
 
-       $user_id = Auth::user()->id ; 
-       if (reservation::where('user_id', '=', $user_id )->
-                        where('trip_id', '=', $id )->count() > 0) {
-                            echo "<script>alert('You have already reserved this trip !');</script>";
-                            $reserve = reservation::All();
-                            $users = User::all();
-                            $trip = trip::find($id);
-                            return view('reservations',compact('reserve','users','trip'));
-; 
-        }else{
-        $reserve = new reservation();
+        $user_id = Auth::user()->id;
+        if (reservation::where('user_id', '=', $user_id)->
+            where('trip_id', '=', $id)->count() > 0) {
+            echo "<script>alert('You have already reserved this trip !');</script>";
+            $reserve = reservation::All();
+            $users = User::all();
+            $trip = trip::find($id);
+            return view('reservations', compact('reserve', 'users', 'trip'));;
+        } else {
 
-        $reserve->trip_id = $trip->id ;
-        $reserve->user_id = $user_id;
-        $reserve->reserve = '1' ;
-        $reserve->number = $request->input('number');
-        $reserve->phone = $request->input('phone');
+            if (request()->ajax()) {
+                $reserve = new reservation();
 
-        $reserve->adult = $request->get('adults');
-        $reserve->child = $request->get('childs');
-        $reserve->senior = $request->get('senior');
+                $reserve->trip_id = $trip->id;
+                $reserve->user_id = $user_id;
+                $reserve->reserve = '1';
 
-        $package = $request->get('package');
-        $reserve->cost = $this->calcPrice($package, $id);
+//                $reserve->phone = $request->input('phone');
+                $reserve->phone = 82398768;
 
-        $reserve->save();
-        $reserve = reservation::All();
-        $users = User::all();
-        $trip = trip::find($id);
-  
-        return response()->json(['success'=>'You are accepted in this trip', 'trip'=> $trip]);
+                $reserve->adult = $request->adults;
+                $reserve->child = $request->childs;
+                $reserve->senior = $request->senior;
 
-        // return view('reservations',compact('reserve','users','trip'));
-    }
+                $package = array($request->adults, $request->childs, $request->senior); //quantity of each of them
+                $reserve->number = array_sum($package); //number of tourists
+
+                $reserve->cost = $this->calcPrice($package, $id); //total cost
+
+                $reserve->save();
+                $reserve = reservation::All();
+                $users = User::all();
+                $trip = trip::find($id);
+
+                return response()->json(
+                    ['success' => 'You are accepted in this trip',
+                        'trip' => $trip,
+                        'reserve' => $reserve
+                    ]);
+
+            } else {
+
+                return response()->json(['error' => 'json data is missing']);
+
+            }
+
+
+            // return view('reservations',compact('reserve','users','trip'));
+        }
     }
 
 
     public function calcPrice($package, $id)
     {
-        $trip = DB::select('select * from trip where id='.$id.'');
+//        $trip = DB::select('select * from trip where id=' . $id . '');
+        $trip = Trip::find($id);
 
-
-        
-// Example 1
+        if (!empty($trip)) {
+            // Example 1
 // $pizza  = "3xadult+2xchild+2xsenior";
-$pieces = explode("+", $package);
-$arr = [];
-$arr_cost = ["adult"=>$trip->adult, "child"=>$trip->child, "senior"=>$trip->senior];
 
-for($i=0; $i<3; $i++)
-{
-	$unit_cost = explode("x", $pieces[$i]);
-	$left = $unit_cost[0];
-	$right = $unit_cost[1];
-	$arr[$i] = $left * $arr_cost[$right];	
-}
+            $arr_cost = [$trip->adult, $trip->child, $trip->senior];
+
+
+//            $pieces = explode("+", $package);
+            $arr = [];
+//            $arr_cost = ["adult" => $trip->adult, "child" => $trip->child, "senior" => $trip->senior];
+
+            for ($i = 0; $i < 3; $i++) {
+//                $unit_cost = explode("x", $pieces[$i]);
+//                $left = $unit_cost[0];
+//                $right = $unit_cost[1];
+                $arr[$i] = $package[$i] * $arr_cost[$i];
+            }
 
 // print_r($arr);
 
-$totoal_cost = $arr[0]+$arr[1]+ $arr[2];
+            $totoal_cost = $arr[0] + $arr[1] + $arr[2];
 
-DB::insert('insert into pricing (package, trip_id, currency ,cost) values (?, ?, ?, ?)', [$package, $id, 'dollar', $totoal_cost]);
+            $final_package = $package[0]."xadult+". $package[1]."xchild+".$package[2]."xsenior";
+            DB::insert('insert into pricing (package, trip_id, currency ,cost) values (?, ?, ?, ?)', [$final_package, $id, 'dollar', $totoal_cost]);
 
 
+            return $totoal_cost;
 
-return $totoal_cost;
-
+        } else {
+            return 0;
+        }
 
 
     }
 
 
-    public function getreservationDetails($id){
-        $trip = DB::select('select * from trip where id='.$id.'');
-        $pricing = DB::select('select * from pricing where trip_id='.$id.'');
+    public function getreservationDetails($id)
+    {
+        $trip = DB::select('select * from trip where id=' . $id . '');
+        $pricing = DB::select('select * from pricing where trip_id=' . $id . '');
 
-        return response()->json(['trip'=> $trip, 'pricing' => $pricing]);
+        return response()->json(['trip' => $trip, 'pricing' => $pricing]);
 
     }
 
@@ -134,37 +156,39 @@ return $totoal_cost;
         $reserve = reservation::All();
         $users = User::all();
         $trip = trip::find($id);
-            
- 
+
+
     }
+
     public function allreservation()
     {
         $reserve = reservation::All();
         $users = User::all();
 
-/*         $reserve = DB::table('reservation')
-                    ->join('users','reservation.user_id','users.id')
-                    ->join('trip','reservation.trip_id','trip.id')
-                    ->get(); */
-        return view('allreservation',compact('reserve','users'));
+        /*         $reserve = DB::table('reservation')
+                            ->join('users','reservation.user_id','users.id')
+                            ->join('trip','reservation.trip_id','trip.id')
+                            ->get(); */
+        return view('allreservation', compact('reserve', 'users'));
 
     }
+
     public function userreservation()
     {
         $users = User::all();
 
-        $user_id = Auth::user()->id ; 
-        $reserve = reservation::where('user_id', '=', $user_id )->get();;
+        $user_id = Auth::user()->id;
+        $reserve = reservation::where('user_id', '=', $user_id)->get();;
 
 
         //$someVariable = Input::get("some_variable");
         //$results = DB::select( DB::raw("SELECT * FROM reservation WHERE user_id = '$user_id'") );
-/*         $reserve = DB::table('reservation')
-                    ->join('users','reservation.user_id','users.id')
-                    ->join('trip','reservation.trip_id','trip.id')
-                    ->get(); */
+        /*         $reserve = DB::table('reservation')
+                            ->join('users','reservation.user_id','users.id')
+                            ->join('trip','reservation.trip_id','trip.id')
+                            ->get(); */
         //echo $results ;
-        return view('userreservation',compact('reserve','users'));
+        return view('userreservation', compact('reserve', 'users'));
 
     }
 
--- public/reservation/js/reserve.js
+++ public/reservation/js/reserve.js
@@ -1,41 +1,31 @@
 $(document).ready(function () {
 
 
-
     // var form = new formData();
     // var reservationData;
 
     formHandler();
 
-    
-
-
-
-
 
 });
 
 
-function confirmDetails(id) {
+function confirmDetails(id, response) {
 
-    
-    // $.ajaxSetup({
-    //     headers: {
-    //         'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-    //     }
-    // });
+
+    console.log(response);
 
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            
+
             var data = JSON.parse(this.responseText);
-            
+
             console.log(data['trip'][0]);
             displayResult(data);
-            
-            
+
+
             //  CallDetails(data);
             // document.getElementById("demo").innerHTML = myArr;
             // console.log(this.responseText);
@@ -44,29 +34,25 @@ function confirmDetails(id) {
     xmlhttp.open("GET", '/reservationdetails/' + id, true);
     xmlhttp.send();
 
-    
 
     // jQuery.ajax({
-    //     url: '/reservationdetails/'+id,  
+    //     url: '/reservationdetails/'+id,
     //     method: 'get',
 
     //     success: function (result) {
 
-    //         data =JSON.parse(result) ;            
+    //         data =JSON.parse(result) ;
     //         console.log(data);
     //     }
     // });
-
-
-
 
 
 }
 
 function formHandler() {
     var id = $("#trip_id").val();
-    $("#next1").click(function () {
 
+    $("#next1").click(function () {
 
 
         var adults = $("#cost0").text();
@@ -83,53 +69,33 @@ function formHandler() {
         }
 
 
-
-        var id = $("#trip_id").val();
-        confirmDetails(parseInt(id));
-
-        // var id = $("#trip_id").val();
-        // if (id !== null) {
-        //     var reservationData = confirmDetails(parseInt(id));
-
-        //     console.log(reservationData);
-        //     var trip_data = reservationData['trip'][0];
-        //     var pricing_data = reservationData['pricing'][0];
-
-        //     console.log(trip_data);
-        //     console.log(pricing_data);
-        // }
-
-
-
-
-
         $.ajaxSetup({
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content'),
-                'Content-Type': 'application/json'
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
 
 
-        // form.append('adults', parseInt($("#cost0").text()));
-        // form.append('childs', parseInt($("#cost1").text()));
-        // form.append('senior', parseInt($("#cost2").text()));
-
-        // form.append('package', package);
-
-        jQuery.ajax({
+        $.ajax({
             url: '/confirm/' + id,
-            method: 'post',
+            type: 'POST',
+            dataType: 'json',
             data: {
+
                 'adults': parseInt($("#cost0").text()),
                 'childs': parseInt($("#cost1").text()),
                 'senior': parseInt($("#cost2").text()),
-                'package': package
+                // 'package':
+                // 'package': parseInt(adults) + "xadult+" + parseInt(childs) + "xchild+" + parseInt(senior) + "xsenior"
             },
-            processData: false,
-            contentType: false,
+
             success: function (result) {
+                var data = result;
+                var id = $("#trip_id").val();
+                confirmDetails(parseInt(id), data);
                 console.log(result);
+
+
             }
         });
 
@@ -139,7 +105,7 @@ function formHandler() {
 }
 
 
-function displayResult(data){
+function displayResult(data) {
     var id = $("#trip_id").val();
     if (id !== null) {
         // var reservationData = confirmDetails(parseInt(id));
