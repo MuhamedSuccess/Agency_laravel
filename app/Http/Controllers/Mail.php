c99851cfd6a619a2eb6d04d4fd2de6b1379c3c5b
Mohamed Adel <mohamedadel2015ar@gmail.com>
2020-07-26 20:37:27 +0200

    add map, mail, functionality and also trip reservation is almost done. also I have been fixed profile navigaton  issues

---
+++ app/Http/Controllers/Mail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Trip;
+use App\Models\Guide;
+use App\Models\User;
+
+class Mail extends Controller
+{
+
+}
